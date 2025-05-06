eec2f9eac1b99abb79b301f9abe7bfd2f779f81c
Apziol <axjoheal@gmail.com>
2025-05-05 21:04:37 -0600

    Avance definitivo?

--- Scripts/Lectura.js
+++ Scripts/Lectura.js
@@ -1,3 +1,73 @@
+window.onload=function(){
+    listar_fases
+
+    const inputIdProyecto = document.getElementById("inputIdProyecto");
+    const inputNombre = document.getElementById("inputNombre");
+  const inputPromedio = document.getElementById("inputPromedio");
+  const inputIdEquipo = document.getElementById("inputIdEquipo");
+  const inputIdCategoria = document.getElementById("inputIdCategoria");
+  const inputIdInvestigador = document.getElementById("inputIdInvestigador");
+  const inputIdArchivo = document.getElementById("inputIdArchivo");
+  const inputIdFase = document.getElementById("inputIdFase");
+  const inputIdEstado = document.getElementById("inputIdEstado");
+  const inputIdCalificacion = document.getElementById("inputIdCalificacion");
+  const btnConfirm = document.getElementById("btnConfirm");
+
+
+  btnConfirm.onclick = function () {
+    const anio = document.getElementById('anio').value;
+    const mes = document.getElementById('mes').value.padStart(2, '0');
+    const dia = document.getElementById('dia').value.padStart(2, '0');
+    const inputFecha = `${anio}-${mes}-${dia}`;
+
+    
+
+    // Validar campos
+    if (
+        inputNombre.value === "" ||
+        inputPromedio.value === "" ||
+        anio === "" || mes === "" || dia === "" ||
+        inputIdEquipo.value === "" ||
+        inputIdCategoria.value === ""||
+        inputIdInvestigador.value === ""||
+        inputIdArchivo.value === ""||
+        inputIdFase.value === ""||
+        inputIdEstado.value === ""||
+        inputIdCalificacion.value === "" ||
+        inputIdProyecto.value === ""
+              ) {
+        alert("Ingrese la información en todos los campos");
+        return;
+    }
+
+    // Enviar datos
+    fetch("https://scitrackapi-production.up.railway.app/api/proyecto/"+inputIdProyecto.value, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nombre: inputNombre.value,
+            fechaRegistro: inputFecha,
+            Equipo_idEquipo: inputIdEquipo,
+            Categoria_idCategoria: inputIdCategoria,
+            Ivestigador_idInvestigador: inputIdInvestigador,
+            Archivos_idArchivos: inputIdArchivo,
+            EstadosProyecto_idEstadosProyecto: inputIdEstado,
+            Calificacion_idCalificacion: inputIdCalificacion
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        alert('Se ha registrado la fase.');
+        window.location.reload();
+    })
+    .catch((error) => console.error("Error:", error));
+};
+}
+
+
 function listar_fases() {
     fetch("https://scitrackapi-production.up.railway.app/api/proyecto/", {
         method: "GET",
@@ -34,5 +104,3 @@ function listar_fases() {
 
 
 
-// Ejecutar al cargar la página
-window.onload = listar_fases;
