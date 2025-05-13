41f2c564f76f925cd7a309a06f31b20cc1f4ad80
roy <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-05-13 07:18:31 -0600

    Primeros componentes

--- scitrack-vue/src/App.vue
+++ scitrack-vue/src/App.vue
@@ -2,27 +2,20 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color=#062A78
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <div class="d-flex justify-center py-2 px-3 my-2 hidden-sm-and-down">
 
         <v-img
           alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
+          class="mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          src="./assets/SCITRACK-logo-white.png"
           width="100"
         />
+
       </div>
 
       <v-spacer></v-spacer>
@@ -36,25 +29,12 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <SideBarBase/>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+<script setup>
+import SideBarBase from '@/components/SideBarBase.vue';
 </script>
---
+++ scitrack-vue/src/components/SideBarBase.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-navigation-drawer color="#2C58B5" dark>
+    <div class="d-flex justify-center py-0 px-0 mb-2 hidden-sm-and-down">
+      <v-img
+        src="../assets/ICTI-logo-white.png"
+        alt="ICTI Logo"
+        contain
+        width="100"
+        style="background-color: #18439E;"
+      />
+    </div>
+
+     <v-list dense>
+      <template v-for="(item, idx) in options">
+        <v-list-item
+          :key="`item-${idx}`"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.message }}</v-list-item-title>
+        </v-list-item>
+        <v-divider
+          v-if="idx < options.length - 1"
+          :key="`divider-${idx}`"
+        />
+      </template>
+    </v-list>
+   
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const options = ref([
+  { message: 'Inicio', id:'btnInicio', icon:'mdi-home' },
+  { message: 'Convocatorias', id:'btnInicio', icon:'mdi-file-document-multiple-outline' },
+  { message: 'Rúbricas', id:'btnInicio', icon:'mdi-file-sign' },
+  { message: 'Proyectos y Usuarios', id:'btnInicio', icon:'mdi-lightbulb-outline' },
+  { message: 'Reportes', id:'btnInicio', icon:'mdi-finance' }
+
+])
+</script>
