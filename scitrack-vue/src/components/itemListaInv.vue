5c20b6d6188f7add262d6a27189a3bb69d012617
roy <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-06-01 00:18:09 -0600

    Agregado nombres completos a los participantes e investigadores en su listado

--- scitrack-vue/src/components/itemListaInv.vue
+++ scitrack-vue/src/components/itemListaInv.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rounded" style="margin-top: 0.666vw; padding: 1vw; padding-left:1.33vw; padding-right: 1.33vw; background-color: #ffffff;">
-    <label style="font-size: small; font-weight: bold;" >Nombre:</label> <label style="font-size: small;"> {{ nombre }}</label> <br>
+    <label style="font-size: small; font-weight: bold;" >Nombre:</label> <label style="font-size: small;"> {{ `${nombre} ${investigador.apellidoPaterno} ${investigador.apellidoMaterno}` }}</label> <br>
     <label style="font-size: small; font-weight: bold;">Institución:</label> <label style="font-size: small;" > {{ institucion }}</label> <br>
     <label style="font-size: small; font-weight: bold;">Conocimiento: </label> <label style="font-size: small;"> {{ conocimiento }}</label> <br>
     <label style="font-size: small; font-weight: bold;">Estado: </label> <label style="font-size: small; color: darkred;" v-if="estado =='Pendiente de revisión'" > {{ estado }}</label>  <label style="font-size: small; color: #35B592;" v-if="estado =='Aprobado'" > {{ estado }}</label> <br>
